5a473bea3e3035be6c225957065480b29b500922
Daniil Khoroshev <daniil.khoroshev@netcracker.com>
2023-02-26 19:41:04 +0400

    add almost done lab 2

--- CG_lab1/MyImage.py
+++ CG_lab1/MyImage.py
@@ -36,6 +36,62 @@ class MyImage:
         if 0 <= x < self.width and 0 <= y < self.height:
             return True
         return False
+    @staticmethod
+    def fox_prepare(x, y):
+        if isinstance(x, list) and isinstance(y, list):
+            return [int(coord * 7 + 500) for coord in x], [int(-coord * 7 + 700) for coord in y]
+        return int(x * 7 + 500), int(-y * 7 + 700)
+
+    @staticmethod
+    def deer_prepare(x, y):
+        if isinstance(x, list) and isinstance(y, list):
+            return [int(coord * 0.4 + 500) for coord in x], [int(-coord * 0.4 + 700) for coord in y]
+        return int(x * 0.4 + 500), int(-y * 0.4 + 700)
+
+    @staticmethod
+    def rabbit_prepare(x, y):
+        if isinstance(x, list) and isinstance(y, list):
+            return [int(coord * 4500 + 500) for coord in x], [int(-coord * 4500 + 700) for coord in y]
+        return int(x * 4500 + 500), int(-y * 4500 + 700)
+
+    def get_bicentric_coordinates(self, x: int, y: int, x0: float, y0: float,
+                                  x1: float, y1: float, x2: float, y2: float):
+        try:
+            lambda0 = ((x1 - x2) * (y - y2) - (y1 - y2) * (x - x2)) / ((x1 - x2) * (y0 - y2) - (y1 - y2) * (x0 - x2))
+            lambda1 = ((x2 - x0) * (y - y0) - (y2 - y0) * (x - x0)) / ((x2 - x0) * (y1 - y0) - (y2 - y0) * (x1 - x0))
+            lambda2 = ((x0 - x1) * (y - y1) - (y0 - y1) * (x - x1)) / ((x0 - x1) * (y2 - y1) - (y0 - y1) * (x2 - x1))
+            # if lambda0 + lambda1 + lambda2 == 1.0:
+            return lambda0, lambda1, lambda2
+        except ZeroDivisionError as zero_error:
+            return 0, 0, 0
+        # else:
+        #     print("lambda0 + lambda1 + lambda2 != 1.0")
+        #     exit(1)
+
+    def get_triangle_bounding_box(self, x0, y0, x1, y1, x2, y2):
+        xmin = min(x0, x1, x2)
+        ymin = min(y0, y1, y2)
+        xmax = max(x0, x1, x2)
+        ymax = max(y0, y1, y2)
+
+        if xmin < 0: xmin = 0
+        if ymin < 0: ymin = 0
+        if xmax > self.width: xmax = self.width
+        if ymax > self.height: ymax = self.height
+        return int(xmin), int(ymin), int(xmax), int(ymax)
+
+    def fill_triangle(self, triangle: List, color: Tuple[int, int, int] = None):
+        x0, y0, x1, y1, x2, y2 = triangle
+        if color is None:
+            color = (255, 255, 255)
+        xmin, ymin, xmax, ymax = self.get_triangle_bounding_box(x0, y0, x1, y1, x2, y2)
+        for x in range(xmin, xmax):
+            for y in range(ymin, ymax):
+                l1, l2, l3 = self.get_bicentric_coordinates(x, y, x0, y0, x1, y1, x2, y2)
+                if l1 > 0 and l2 > 0 and l3 > 0:
+                    self.set_pixel(x, y, color)
+
+
 
 
 def line_1(x0: int, y0: int, x1: int, y1: int, img: MyImage, color: Tuple[int, int, int], point_num: int = 100):
@@ -45,13 +101,11 @@ def line_1(x0: int, y0: int, x1: int, y1: int, img: MyImage, color: Tuple[int, i
         y = int(y0 * (1.0 - t) + y1 * t)
         img.set_pixel(x, y, color)
 
-
 def line_2(x0: int, y0: int, x1: int, y1: int, img: MyImage, color: Tuple[int, int, int]):
     for x in range(x0, x1 + 1):
         t = (x - x0) / (x1 - x0)
         y = int(y0 * (1.0 - t) + y1 * t)
         img.set_pixel(x, y, color)
-
 
 def line_3(x0: int, y0: int, x1: int, y1: int, img: MyImage, color: Tuple[int, int, int]):
     step = False
@@ -81,15 +135,12 @@ def line_4(x0: int, y0: int, x1: int, y1: int, img: MyImage, color: Tuple[int, i
     if dx != 0:
         d_error = abs(dy / dx)
     else:
-        d_error = 0
+        d_error = 0.0
     error = 0.0
     y = y0
 
     for x in range(x0, x1 + 1):
-        if step:
-            img.set_pixel(y, x, color)
-        else:
-            img.set_pixel(x, y, color)
+        img.set_pixel(y, x, color) if step else img.set_pixel(x, y, color)
         error += d_error
         if error > 0.5:
             y += 1 if y1 > y0 else -1
@@ -153,16 +204,13 @@ def test_obj_model(file_path: str, prepare = None, model_name: str = 'Obj model'
     image = MyImage(h, w)
     color = (255, 255, 255)
 
-    if prepare is None:
-        prepare = lambda x: int(x * 7 + 400)
-
     points, faces = read_obj(file_path)
     faces = np.array(faces, dtype=int)
     faces -= 1
 
     for point in points:
         x, y = point[0], point[1]
-        x, y = prepare(x), prepare(y)
+        x, y = prepare(x, y)
 
         if not image.point_exist(x, y):
             continue
@@ -179,14 +227,59 @@ def test_obj_model(file_path: str, prepare = None, model_name: str = 'Obj model'
         x1, y1, _ = points[point_1_id]
         x2, y2, _ = points[point_2_id]
 
-        x0, y0 = prepare(x0), prepare(y0)
-        x1, y1 = prepare(x1), prepare(y1)
-        x2, y2 = prepare(x2), prepare(y2)
+        x0, y0 = prepare(x0, y0)
+        x1, y1 = prepare(x1, y1)
+        x2, y2 = prepare(x2, y2)
 
         if not (image.point_exist(x0, y0) or image.point_exist(x1, y1) or image.point_exist(x2, y2)):
             continue
         line_4(x0, y0, x1, y1, image, color)
         line_4(x0, y0, x2, y2, image, color)
         line_4(x1, y1, x2, y2, image, color)
+
+    image.show(f'{model_name} faces')
+
+def test_fill_triangle():
+    img = MyImage(12, 12)
+
+    triangle_1 = [2.5, 1.5, 5.7, 10.4, 10.3, 5.5]
+    triangle_2 = [0, 0, 11, 0, 13, 13]
+
+    img.fill_triangle(triangle_1)
+    img.show('Triangle #1')
+    img.clear()
+
+    img.fill_triangle(triangle_2)
+    img.show('Triangle #2')
+    img.clear()
+
+def test_fill_obj_model(file_path: str, prepare = None, model_name: str = 'Obj model'):
+    h, w = 1000, 1000
+    image = MyImage(h, w)
+    color = (255, 255, 255)
+
+    points, faces = read_obj(file_path)
+    faces = np.array(faces, dtype=int)
+    faces -= 1
+
+    for face in faces:
+        if len(face) != 3:
+            continue
+        point_0_id, point_1_id, point_2_id = face[0], face[1], face[2]
+
+        x0, y0, _ = points[point_0_id]
+        x1, y1, _ = points[point_1_id]
+        x2, y2, _ = points[point_2_id]
+
+        x0, y0 = prepare(x0, y0)
+        x1, y1 = prepare(x1, y1)
+        x2, y2 = prepare(x2, y2)
+
+        color = (np.random.randint(0, 255, dtype=np.uint8),
+                 np.random.randint(0, 255, dtype=np.uint8),
+                 np.random.randint(0, 255, dtype=np.uint8))
+        triangle = [x0, y0, x1, y1, x2, y2]
+
+        image.fill_triangle(triangle, color)
 
     image.show(f'{model_name} faces')
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from CG_lab1 import test, MyImage
+from CG_lab1 import *
+from CG_lab1.MyImage import *
 
 
 if __name__ == "__main__":
+    # lab №1
     # №1
     # test.test()
 
     # №2, №3
-    MyImage.test_lines()
+    # MyImage.test_lines()
 
     # №4, №5, №6, №7
-    # file_path = './obj_files/deer.obj'
-    # fox_lambda = lambda x: int(x * 7 + 300)
-    # rabbit_lambda = lambda x: int(x * 4000 + 400)
-    # deer_lambda = lambda x: int(x * 0.40 + 400)
-    # MyImage.test_obj_model(file_path, prepare=deer_lambda)
+    # file_path = './obj_files/rabbit.obj'
+    # MyImage.test_obj_model(file_path, prepare=MyImage.MyImage.rabbit_prepare)
+
+    # lab №2
+    # MyImage.test_fill_triangle()
+    file_path = './obj_files/deer.obj'
+    test_fill_obj_model(file_path, prepare=MyImage.deer_prepare, model_name='deer')
